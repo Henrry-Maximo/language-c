abc07cae5ec5c5939c6cf53ec5000480048fce9a
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-09-21 23:17:54 -0300

    🎉  First Commit!

---
+++ aula-1-1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // informar o que iremos utilizar
+#include <stdlib.h>
+
+int main() {
+    // código que estiver entre as chaves será executado pela máquina
+
+    printf("Hello, world!\n"); // uma função de stdio para imprimir algo no terminal
+    system("pause"); // aguardar o usuário pressionar alguma tecla para continuar
+
+    return 0; // programa encerrou sem erros
+}
