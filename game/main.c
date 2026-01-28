54376b995951410b89801d11f434c11550c6284b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-27 22:44:42 -0300

    ✨ Feat: Add validate board player win

--- game/main.c
+++ game/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void zeraTabuleiro(char tabuleiro[3][3])
+void clearBoard(char board[3][3])
 {
     int i;
     int j;
@@ -10,12 +10,12 @@ void zeraTabuleiro(char tabuleiro[3][3])
     {
         for (j = 0; j < 3; j++)
         {
-            tabuleiro[i][j] = ' ';
+            board[i][j] = ' ';
         }
     }
 };
 
-void exibeTabuleiro(char tabuleiro[3][3])
+void displayBoard(char board[3][3])
 {
     int i;
     int j;
@@ -24,7 +24,7 @@ void exibeTabuleiro(char tabuleiro[3][3])
     {
         for (j = 0; j < 3; j++)
         {
-            printf("%c |", tabuleiro[i][j]);
+            printf("%c |", board[i][j]);
         }
 
         printf("\n");
@@ -32,23 +32,23 @@ void exibeTabuleiro(char tabuleiro[3][3])
     printf("\n\n");
 };
 
-void jogada(char tabuleiro[3][3], char player)
+void play(char board[3][3], char player)
 {
     int x, y;
 
     while (1)
     {
-        printf("Entre com as coordenadas: ");
+        printf("Enter the coordinates: ");
         scanf("%d %d", &x, &y);
 
         // usuário digitou uma posição que não existe
         if ((x > 2 || y > 2) || (x < 0 || y < 0))
         {
-            printf("Coordenadas invalidas\n");
+            printf("Invalid Coordinates\n");
         }
-        else if (tabuleiro[x][y] == ' ')
+        else if (board[x][y] == ' ')
         {
-            tabuleiro[x][y] = player;
+            board[x][y] = player;
             break;
         }
         else
@@ -56,28 +56,78 @@ void jogada(char tabuleiro[3][3], char player)
             printf("Essa posicao nao pode ser utilizada\n");
         }
 
-        // tabuleiro[x][y] = c1;
+        // board[x][y] = c1;
     }
+}
+
+int validateBoard(char board[3][3], char player)
+{
+    int i;
+    int j;
+
+    // validação por linhas
+    for (i = 0; i < 3; i++)
+    {
+        if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+        {
+            return 1;
+        }
+    }
+
+    // validação por colunas
+    for (i = 0; i < 3; i++)
+    {
+        if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j])
+        {
+            return 1;
+        }
+    }
+
+    // validação de diagonais
+    if (board[0][0] != ' '
+            && board[0][0] == board[1][1]
+            && board[1][1] == board[2][2])
+    {
+        return 1;
+    }
+
+    if (board[0][2] != ' '
+            && board[0][2] == board[1][1]
+            && board[1][1] == board[2][0])
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 int main()
 {
-    char tabuleiro[3][3];
+    char board[3][3];
     char c1 = 'X';
     char c2 = 'O';
     char player = c1;
 
-    zeraTabuleiro(tabuleiro);
-    exibeTabuleiro(tabuleiro);
+    clearBoard(board);
+    displayBoard(board);
 
     while (1)
     {
-        jogada(tabuleiro, player);
-        // quando usamos uma linha embaixo do bloco if, podemos remover as chaves
-        if (player == c1) player = c2;
-        else player = c1;
-        exibeTabuleiro(tabuleiro);
+        play(board, player);
+        displayBoard(board);
+        if (validateBoard(board, player) == 1)
+        {
+            printf("O player %c VENCEU!\n", player);
+            break;
+        }
+        else
+        {
+            // quando usamos uma linha embaixo do bloco if, podemos remover as chaves
+            if (player == c1) player = c2;
+            else player = c1;
+        }
     }
 
+    printf("END GAME");
     return 0;
 };
